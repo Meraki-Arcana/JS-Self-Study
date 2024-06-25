b3d0fa39e0efb60a669f00eb0cb9dbc11d7c8602
Meraki-Arcana <146993515+Meraki-Arcana@users.noreply.github.com>
2024-06-25 15:17:49 -0600

    Add files via upload

---
+++ mathematical-operators.js
@@ -0,0 +1,27 @@
+// Mathematical Operators - Javascript In Depth (Part One)
+
+// Addition
+
+let age = 10 + 23;
+console.log(age) //33
+
+// Subtraction 
+let points = 50 - 19;
+console.log(points);
+
+// Multiplication
+let score = 7 * 50;
+console.log(score);
+
+//Division
+let divide = 100 / 13;
+console.log(divide);
+
+//Modulus (Remainder)
+let mod = 6 % 3;
+console.log(mod);
+
+// Exponentiation (exponents)
+
+let exp = 4**2; // 4 to the power of 2.
+console.log(exp);
