80fc56ea3910288b10a99e65e1d39ebb6ecdeb73
Meraki-Arcana <146993515+Meraki-Arcana@users.noreply.github.com>
2024-06-25 17:15:09 -0600

    Add files via upload

---
+++ increment-decrement-operators.js
@@ -0,0 +1,45 @@
+// Mathematical Operators - Javascript In Depth (Part Two)
+
+// Increment & Decrement Assignment Operators
+
+/*
+
+    —— ++ Increment by 1
+    —— -- Decrement by 1
+    —— += Increment by value
+    —— -+ Decrement by value
+    —— *= Multiply by value
+    —— /= Divide by value
+    —— %= Modulus by value
+    —— **= Exponentiate by value
+*/
+
+let points = 10;
+points++ ; // increment points by 1; or 10 + 11.
+console.log(points);
+
+let score = 20;
+score--; //decrement score by 1; or 20 - 1.
+console.log(score);
+
+score += 6; //add 6 to the previous value; or 19 + 6.
+console.log(score);
+
+score += 100;//add 100 to the previous value; or 25 + 100.
+console.log(score);
+
+score -= 100; //subtract 100 from the previous value; or 125 - 100.
+console.log(score);
+
+let age = 7;
+age *= 2; // age = age * 2; or 7 * 2.
+console.log(age);
+
+age /= 2; // age = age / 2; or 14 / 2.
+console.log(age);
+
+age %= 7; // age = age % 7; or 7 divided by 7.
+console.log(age);
+
+age **= 14;
+console.log(age);
