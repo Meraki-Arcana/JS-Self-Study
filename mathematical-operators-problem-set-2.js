f09a8c7707e9ee42e45eb8888d4d0dc6b4968a55
Meraki-Arcana <146993515+Meraki-Arcana@users.noreply.github.com>
2024-06-26 14:44:09 -0600

    Add files via upload

---
+++ mathematical-operators-problem-set-2.js
@@ -0,0 +1,27 @@
+/*
+    1. Create a let variable called "points" and print it out.
+    2. Assign "points" the number 20 and print it out.
+    3. Add 15 to "points" and print it out.
+
+    Question1: What are all the ways you can achieve step 3?
+    Question2: What would happen if you tried to do this with a const variable?
+
+*/
+
+let points;
+console.log(points);
+
+points = 20;
+console.log(points);
+
+points += 15; //Option 1
+console.log(points);
+
+/* Option 2
+
+points = points + 15;
+console.log(points);
+
+Is another way of achieving step 3.
+
+*/
---
+++ mathematical-operators-warmup.js
@@ -0,0 +1,18 @@
+/*
+    1. Use let to create a variable that points to a String.
+    —— Then add another string to this same variable and print it out.
+    2.Use let to create a variable that points to a number.
+    —— Then add another number to this same variable and print it out.
+
+    HINT: Re-assigning the variable is not the goal, but adding 
+    */
+
+    let name = "Joey";
+    name = name + " is racing today.";
+    console.log(name);
+
+    
+    let points = 21;
+    points += 10;
+    console.log(points);
+    
