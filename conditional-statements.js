2797dd6d17c6c2fe86c0910fe9b1bc7eac14a3d0
Meraki-Arcana <146993515+Meraki-Arcana@users.noreply.github.com>
2024-06-27 16:51:11 -0600

    Add files via upload

---
+++ conditional-statements.js
@@ -0,0 +1,65 @@
+let happy = true;
+
+if (happy === true) {
+  console.log("Yay!");
+}
+
+let points = 10;
+
+if (points > 5) {
+  console.log("You have more than 5 points!");
+}
+
+console.log("---------");
+// I'm asking, is 10 greater than 5? Yes, so the console will print the statement I put into the code block.
+
+let sushiPcs = 10;
+
+if (sushiPcs > 5) {
+  console.log("You have more than 5 pieces of sushi.");
+} else {
+  console.log("You can buy more sushi pieces!");
+}
+
+console.log("That's a good meal :)");
+
+console.log("---------");
+
+let score = 20;
+
+if (score > 30) {
+  console.log("You scored above 30!");
+} else if (score > 20) {
+  console.log("Your score is between 20 and 30.");
+}
+
+console.log("You have very little points...");
+
+//Nothing gets executed into the console because 20 is not greater than 20, nor is 20 greater than 30.
+
+console.log("---------");
+
+let health = 40;
+
+if (health > 50) {
+  console.log("You have more than 50% health!");
+} else if (health > 20);
+{
+  console.log("You are running low on health!");
+}
+
+console.log("*Use health potion*");
+
+console.log("---------");
+
+let newScore = 20;
+
+if (newScore > 50) {
+  console.log("You scored above 50!");
+} else if (newScore > 20) {
+  console.log("Your score is between 20 and 30.");
+} else {
+  console.log("You scored less than 20.");
+}
+
+console.log("You can do better!");
