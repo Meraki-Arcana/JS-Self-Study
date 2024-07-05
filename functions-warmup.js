070c78d9ccb7e77803f487677526de676347849f
Meraki-Arcana <146993515+Meraki-Arcana@users.noreply.github.com>
2024-07-05 17:01:13 -0600

    Add files via upload

---
+++ functions-warmup.js
@@ -0,0 +1,27 @@
+/*
+    Write a function called "chill" that does the following:
+    1. Prints out at the start: "Doing some chilling"
+    2. Then, prints out:
+        "Chill...1"
+        "Chill...2"
+        "Chill...3"
+        ... through
+        "Chill 10"
+    3. Then finally prints out: "That was ice cold."
+
+    When you're done, call the function once.
+*/
+
+//function declaration
+const chill = () => {
+  console.log("Doing some chilling");
+
+  for (let i = 1; i <= 10; i++) {
+    console.log(`Chill... ${i}`);
+  }
+
+  console.log("That as ice cold!");
+};
+
+//Calling the function
+chill();
