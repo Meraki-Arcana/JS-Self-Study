a6fd7d8afff41ac861e977336aa9126550a2d53f
Meraki-Arcana <146993515+Meraki-Arcana@users.noreply.github.com>
2024-07-07 14:48:06 -0600

    Add files via upload

---
+++ arrays-problem-set-2.js
@@ -0,0 +1,13 @@
+/*
+    Create a variable called "greeting" and initialize it to:
+    "Hello, nice to meet you!"
+
+    Use a loop to loop through this string (just like you would an array)
+
+*/
+
+const greeting = "Hello, nice to meet you!";
+
+for(let i = 0; i < greeting.length ; i++) {
+    console.log(greeting[i])
+}
