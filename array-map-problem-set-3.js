764749a48870098c4840ba722576cd4e8cc330a8
Meraki-Arcana <146993515+Meraki-Arcana@users.noreply.github.com>
2024-07-08 15:23:01 -0600

    Add files via upload

---
+++ array-map-problem-set-3.js
@@ -0,0 +1,21 @@
+/*
+    1. Create the following array called "items":
+        ["light", "banana", "phone", "book", "mouse"]
+    2. Create a new array called "plurals" that:
+        -maps over "items" and adds an 's' to each one.
+    3. Print out both arrays.
+    4. Change it so if we see "mouse", e instead return mice.
+*/
+
+
+const items = ["light", "banana", "phone", "book", "mouse"];
+
+const plurals = items.map((item)=>{
+    if(item === "mouse"){
+        return "mice"
+    }
+    return item + 's'
+})
+
+console.log(items);
+console.log(plurals);
