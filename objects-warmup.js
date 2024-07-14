d711b77a85c0cc0b9201618f847e53328fd285a0
Meraki-Arcana <146993515+Meraki-Arcana@users.noreply.github.com>
2024-07-14 15:43:47 -0600

    Add files via upload

---
+++ objects-warmup.js
@@ -0,0 +1,28 @@
+/*
+    1. Create an object called "mexico" with the following properties:
+        - id → 24
+        - name → "Mexico"
+        - capital → "Mexico City"
+        - neighbors → ["USA", "Guatemala", "Belize"]
+    2. Print out "mexico" to the terminal
+    3. Change "id" to be 25
+    4. Add "Honduras" to the list of neighbors of "mexico"
+    5. Print out "mexico" to the terminal again
+
+*/
+
+const mexico = { 
+    id: 24,
+    name: "Mexico",
+    capital: "Mexico City",
+    neighbors: ["USA", "Guatemala", "Belize"]
+}
+
+console.log(mexico); 
+
+mexico["id"] = 25;
+
+mexico.neighbors.push("Honduras");
+console.log(mexico)
+
+
